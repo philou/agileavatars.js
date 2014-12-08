f47d6857a8910fb56063ef357889580aecb9fe95
Philou <philippe.bourgau@free.fr>
2014-12-08 07:10:09 +0100

    Send real mails with Sendgrid

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,38 @@
 var express = require('express');
+var nodemailer = require('nodemailer');
+var emailAddresses = require('email-validator');
+var sendgrid  = require('sendgrid')(process.env.SENDGRID_USERNAME, process.env.SENDGRID_PASSWORD);
 var router = express.Router();
 
 router.get('/', function(req, res) {
   res.render('index', { title: 'AgileAvatars.com' });
+});
+
+router.post('/', function (req, res) {
+  var email, mailOpts, smtpTrans;
+
+  email = req.body.email;
+  if (!emailAddresses.validate(email)) {
+    res.render('index', { title: 'AgileAvatars.com', error: 'the email is invalid', page: 'index' })
+    return;
+  }
+
+  // headers: "From: " + req.body.email,
+  sendgrid.send({
+    to:       'philippe.bourgau@gmail.com',
+    from:     email,
+    subject:  '[Interested by AgileAvatars.com]',
+    text:     "Interested by AgileAvatars.com"
+  }, function(error, json) {
+    if (error) {
+      res.render('index', { title: 'AgileAvatars.com', error: error, page: 'index' })
+      console.log("Failed to send email because " + error);
+    }
+    else {
+      res.render('index', { title: 'AgileAvatars.com', contactSent: true, email: email, page: 'index' })
+      conole.log("Mail sent successfully");
+    }
+  });
 });
 
 module.exports = router;
